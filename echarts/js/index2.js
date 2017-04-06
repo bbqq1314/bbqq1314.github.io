4cc720f007067245e6c5e8d3b426345fdc2c9fe3
bbqq1314 <1164192896@qq.com>
2017-04-06 16:41:47 +0800

    0020

---
+++ echarts/js/index2.js
@@ -0,0 +1,2 @@
+/* index2.js Date:2017-04-06 16:34:54 */
+define("index2.js",[],function(require,a,b){var c={_init:function(){c.abc()},abc:function(){var a=echarts.init(document.getElementById("main"));var b={title:{text:"ECharts \u5c0f\u4f8b\u5b50"},tooltip:{},legend:{data:["\u638c\u63e1\u7a0b\u5ea6"]},xAxis:{data:["photoshop","div+css","js","vue","handlebar"]},yAxis:{},series:[{name:"\u638c\u63e1\u7a0b\u5ea6",type:"bar",data:[30,30,25,15,15]}]};a.setOption(b)}};b.exports={init:c._init}});
